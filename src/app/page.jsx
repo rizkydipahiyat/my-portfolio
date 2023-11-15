3ec4cfbcba1d558fa4d0a4fd2e4dd2c5cafd4348
rizkydipahiyat <rizkydipahiyat76@gmail.com>
2023-11-15 16:30:46 +0700

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
         <meta
           name="description"
           content={
-            "Hello, I am Rizky Dipahiyat Alghipari. A fresh computer science graduate who is trying to specialize in frontend & React web development."
+            "A recent Computer Science graduate with a keen interest in web development"
           }
         />
 
@@ -28,7 +28,7 @@ export default function Home() {
         />
         <meta
           property="og:description"
-          content="Hello, I am Rizky Dipahiyat Alghipari. A fresh computer science graduate who is trying to specialize in frontend & React web development."
+          content="A recent Computer Science graduate with a keen interest in web development"
         />
         <meta
           property="og:image"
@@ -75,8 +75,8 @@ export default function Home() {
           <div className="md:2/3 md:mt-32 md:mb-40 mt-5">
             <div className="md:px-5">
               <span className="md:text-2xl text-md font-light text-justify">
-                A fresh computer science graduate who is trying to specialize in
-                frontend & React web development.
+                A recent Computer Science graduate with a keen interest in web
+                development
               </span>
             </div>
           </div>
