573f8aa87c0e9d84da61e7c6675222cb9b7c67a6
rizkydipahiyat <rizkydipahiyat76@gmail.com>
2024-01-07 12:35:03 +0700

    remove nexttoploader

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { DarkModeProvider } from "@/context/DarkModeContext";
 import Footer from "@/components/Footer/Footer";
-import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +21,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <DarkModeProvider>
-          <NextTopLoader color="#4cd137" showSpinner={false} />
-          {/* <Navbar /> */}
           <Navbar />
           {children}
           <Footer />
